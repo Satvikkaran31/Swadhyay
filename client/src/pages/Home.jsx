2b98ad0e5bca55616554815f205e56576adfbbf9
satvik karan <yourname@example.com>
2025-07-21 14:59:40 +0530

    improved footer and added certifications

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -102,7 +102,7 @@ useEffect(() => {
       viewBox="0 0 14 15"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      class="learnmore__icon-svg"
+      className="learnmore__icon-svg"
       width="10"
     >
       <path
@@ -116,7 +116,7 @@ useEffect(() => {
       fill="none"
       width="10"
       xmlns="http://www.w3.org/2000/svg"
-      class="learnmore__icon-svg learnmore__icon-svg--copy"
+      className="learnmore__icon-svg learnmore__icon-svg--copy"
     >
       <path
         d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
